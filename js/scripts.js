713e6f00cf7f6e7f6f3f4df4a42df26b29e8f1a1
Victoria045 <victoriaberyl45@gmail.com>
2021-09-12 13:49:43 +0300

    implement hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,57 @@
-//backend logic
 function toggle(element, imageContent) {
-  $(element || this).animate({opacity: 0}, 500, function() {
+  $(element || this).animate({ opacity: 0 }, 500, function () {
     $(element).hide();
-    $(imageContent).show().animate({opacity:1}, 500,);
+    $(imageContent).show().animate({ opacity: 1 }, 500);
   });
 }
 
+const overlays = document.querySelectorAll(".overlay-wrapper");
+
+overlays.forEach((overlay) => {
+  overlay.addEventListener(
+    "mouseover",
+    function () {
+      const overlay_cont = overlay.querySelector(".overlay-cont");
+      overlay_cont.style.display = "block";
+    },
+    true
+  );
+
+  overlay.addEventListener(
+    "mouseout",
+    function () {
+      const overlay_cont = overlay.querySelector(".overlay-cont");
+      overlay_cont.style.display = "none";
+    },
+    true
+  );
+});
 
 //user logic
-$(document).ready(function() {
-  $('.img-content').click(function(event) {
-
-    const toggledElement = $(event.target).hasClass('img-content') ? $(event.target) : $(event.target).parent();
+$(document).ready(function () {
+  $(".img-content").click(function (event) {
+    const toggledElement = $(event.target).hasClass("img-content")
+      ? $(event.target)
+      : $(event.target).parent();
     toggle(toggledElement, $(toggledElement).prev());
   });
 
-  $('.img-cont').click(function() {
+  $(".img-cont").click(function () {
     toggle(this, $(this).next());
   });
 
-  $('#myForm').submit(function(e) {
+  $("#myForm").submit(function (e) {
     e.preventDefault();
 
     const myForm = e.target;
-    
-    const formData = new FormData(myForm);
-    $(myForm).find('.myAlert').addClass('alert-success').
-    html(`** ${myForm.fname.value} we have received your message. Thank you for reaching out to us. **`).show();
-  });
 
-  $('.overlay-wrapper').mouseover(function() {
-    $(".overlay-cont").show();
-  });
-  
-  $('.overlay-wrapper').mouseout(function() {
-    $(".overlay-cont").hide();
+    const formData = new FormData(myForm);
+    $(myForm)
+      .find(".myAlert")
+      .addClass("alert-success")
+      .html(
+        `** ${myForm.fname.value} we have received your message. Thank you for reaching out to us. **`
+      )
+      .show();
   });
 });
